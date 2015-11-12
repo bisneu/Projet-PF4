9e73b196ef0c76611c618539c17993c60a8a6ec1
yvaelsehil <virus-92i@hotmail.fr>
2015-11-12 18:31:39 +0100

    teub

--- teub.ml
+++ teub.ml
@@ -1,5 +1,10 @@
 type state = A | D ;;
-type generation;;
-type rule = (state*state*state*state*state);;
-type automaton;;
-print_string "suce ma teub";;
+type generation = (state array) array;;
+type vonNeu1 =(state*state*state*state*state);;
+type vonMoore = (state*state*state*state*state*state*state*state*state);;
+type rule = R1 of ruleVonNeu1;; 
+type automaton = ((rule list)*generation);;
+let parse  = (1,2,3,4);;
+let parse3 = (1,8,3,4);;
+parse = parse3;;
+
